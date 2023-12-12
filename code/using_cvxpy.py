7622e3463f22fddca9a4b4fc116ba58ce3216749
Mattia Guazzaloca <mattia.guazzaloca@gmail.com>
2023-12-12 11:50:04 +0100

    updated cvxpy based solution (see output in output.txt)

--- code/using_cvxpy.py
+++ code/using_cvxpy.py
@@ -3,7 +3,7 @@ import numpy as np
 import dynamics as dyn
 import cost as cst
 
-tf = 10  # final time in seconds
+tf = 5  # final time in seconds
 dt = dyn.dt  # get discretization step from dynamics
 ns = dyn.number_of_states
 ni = dyn.number_of_inputs
@@ -22,15 +22,8 @@ def newton_method_optcon(xx_ref, uu_ref):
     # for k=0,1,...
     kk = 0
 
-    at = np.zeros((ns, TT, max_iters))
-    bt = np.zeros((ni, TT, max_iters))
     fx = np.zeros((ns, ns, TT, max_iters))
     fu = np.zeros((ni, ns, TT, max_iters))
-    lT = np.zeros((max_iters))
-
-    fxx = np.zeros((ns, ns, TT, max_iters))
-    fuu = np.zeros((ni, ni, TT, max_iters))
-    fxu = np.zeros((ni, ns, TT, max_iters))
 
     Qt = np.zeros((ns, ns, TT, max_iters))
     Rt = np.zeros((ni, ni, TT, max_iters))
@@ -38,10 +31,6 @@ def newton_method_optcon(xx_ref, uu_ref):
     qqt = np.zeros((ns, TT, max_iters))
     rrt = np.zeros((ni, TT, max_iters))
 
-    sigma_t = np.zeros((ni, TT, max_iters))
-
-    delta_u = np.zeros((ni, TT, max_iters))
-    delta_x = np.zeros((ns, TT, max_iters))
     print("Initialization done")
 
     for kk in range(max_iters):
@@ -51,7 +40,6 @@ def newton_method_optcon(xx_ref, uu_ref):
             xx[:, :, kk] = xx_ref
             xx[:, 0, kk + 1] = xx_ref[:, 0]
             uu[:, :, kk] = uu_ref
-            delta_x[:, 0, kk] = xx_ref[:, 0]
 
         # Evaluate nabla1f, nabla2f, nabla1cost, nabla2cost, nablaTcost and hessians
         for tt in range(TT - 1):  # da 0 a 9999
@@ -68,54 +56,57 @@ def newton_method_optcon(xx_ref, uu_ref):
 
         print("Qt,k ; Rt,k ; St,k, qt,k, rt,k computed\n")
 
-        # Define the variables
-        delta_x = cp.Variable((ns, TT))
+        # Define the variables with the state augmentated
+        delta_x_tilda = cp.Variable((ns + 1, TT))
         delta_u = cp.Variable((ni, TT))
 
         # Define the objective function
         obj = 0
 
         for tt in range(TT - 1):
-            q = qqt[:, tt, kk]
-            r = rrt[:, tt, kk]
+            q = qqt[:, tt, kk, None]
+            r = rrt[:, tt, kk, None]
             Q = Qt[:, :, tt, kk]
             R = Rt[:, :, tt, kk]
             S = St[:, :, tt, kk]
-            obj += cp.matmul(cp.Constant(np.concatenate([q, r]).T), cp.hstack([delta_x[:, tt], delta_u[:, tt]])) + 0.5 * cp.quad_form(
-                cp.hstack([delta_x[:, tt], delta_u[:, tt]]), cp.Constant(np.vstack((np.hstack((Q, S.T)), np.hstack((S, R)))))
-            )
+            Q_tilda = np.vstack((np.hstack((np.zeros((1, 1)), q.T)), np.hstack((q, Q))))
+            S_tilda = np.hstack((r, S))
+            R_tilda = R
+            quad1 = cp.quad_form(delta_x_tilda[:, tt, None], Q_tilda)
+            quad4 = cp.quad_form(delta_u[:, tt, None], R_tilda)
+            obj += 0.5 * (quad1 + quad4)
 
-        q = cp.Constant(qqt[:, TT - 1, kk].T)
-        Q = cp.Constant(Qt[:, :, TT - 1, kk])
-        obj += 0.5 * cp.quad_form(delta_x[:, TT - 1], Q) + cp.matmul(q, delta_x[:, TT - 1])
+        q = qqt[:, TT - 1, kk, None]
+        Q = Qt[:, :, TT - 1, kk]
+        Q_tilda = np.vstack((np.hstack((np.zeros((1, 1)), q.T)), np.hstack((q, Q))))
+        obj += 0.5 * cp.quad_form(delta_x_tilda[:, TT - 1, None], Q_tilda)
 
         # Define the constraints
         constraints = []
         for tt in range(TT - 1):
-            grad1_f = cp.Constant(fx[:, :, tt, kk].T)
-            grad2_f = cp.Constant(fu[:, :, tt, kk].T)
-            constraints.append(delta_x[:, tt + 1] == cp.matmul(grad1_f, delta_x[:, tt]) + cp.matmul(grad2_f, delta_u[:, tt]))
-        constraints.append(delta_x[:, 0] == 0)
+            A = fx[:, :, tt, kk].T
+            B = fu[:, :, tt, kk].T
+            A_tilda = np.vstack((np.hstack((np.ones((1, 1)), np.zeros((1, ns)))), np.hstack((np.zeros((ns, 1)), A))))
+            B_tilda = np.vstack((np.zeros((1, ni)), B))
+            constraints.append(delta_x_tilda[:, tt + 1] == A_tilda @ delta_x_tilda[:, tt] + B_tilda @ delta_u[:, tt])
+        constraints.append(delta_x_tilda[:, 0, None] == np.vstack((np.ones((1, 1)), np.zeros((ns, 1)))))
 
         # Define the problem
-        problem = cp.Problem(cp.Minimize(obj), constraints)
+        problem = cp.Problem(cp.Minimize(cp.sum(obj)), constraints)
 
         # Solve the problem
         problem.solve()
 
         # Get the optimal values
-        delta_x_star = delta_x.value
         delta_u_star = delta_u.value
-        print("delta_u and delta_x computed \n")
+        print("delta_u computed \n")
 
         # STEP 2: compute the input sequence
         print("Computing the input sequence...")
-        print("delta_x", delta_x[:, :, kk])
-        print("sigma_t", sigma_t[:, :, kk])
 
         stepsize = 0.7
         for tt in range(TT - 1):
-            uu[:, tt, kk + 1] = uu[:, tt, kk] + stepsize * delta_u_star[:, tt, kk]
+            uu[:, tt, kk + 1] = uu[:, tt, kk] + stepsize * delta_u_star[:, tt]
         print("Input sequence computed \n")
 
         # STEP 3: compute the state sequence
